e92a6836b7231a951f33b691cb4b416b1a16816d
rolisanchez <roli.s91@gmail.com>
2020-07-24 16:35:09 +0800

    Testing Moves, Winning and Draw

--- BDDQuickNimble/AppTacToeTests/BoardSpec.swift
+++ BDDQuickNimble/AppTacToeTests/BoardSpec.swift
@@ -32,6 +32,63 @@ import Nimble
 
 class BoardSpec: QuickSpec {
   override func spec() {
+    var board: Board!
+    
+    beforeEach {
+        board = Board()
+    }
+    
+    describe("playing") {
+        context("a single move") {
+            it("should switch to nought") {
+                try! board.playRandom()
+                expect(board.state).to(equal(.playing(.nought)))
+            }
+        }
+        context("two moves") {
+            it("should switch back to cross") {
+                try! board.playRandom()
+                try! board.playRandom()
+                expect(board.state) == .playing(.cross)
+            }
+        }
+        context("a winning move") {
+            it("should switch to won state") {
+                // Arrange
+                try! board.play(at: 0)
+                try! board.play(at: 1)
+                try! board.play(at: 3)
+                try! board.play(at: 2)
+                
+                // Act
+                try! board.play(at: 6)
+                
+                // Assert
+                expect(board.state) == .won(.cross)
+            }
+        }
+        context("a move leaving no remaining moves") {
+            it("should switch to draw state") {
+                // Arrange
+                try! board.play(at: 0)
+                try! board.play(at: 2)
+                try! board.play(at: 1)
+                try! board.play(at: 3)
+                try! board.play(at: 4)
+                try! board.play(at: 8)
+                try! board.play(at: 6)
+                try! board.play(at: 7)
+                
+                // Act
+                try! board.play(at: 5)
+                
+                // Assert
+                expect(board.state) == Board.State.draw
+            }
+        }
+
+
+    }
 
   }
 }
